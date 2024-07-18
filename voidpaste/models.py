216d13b143dc10473d50b7cce78e85c6d04ace35
jpdreamthug <lentorilp@gmail.com>
2024-07-18 22:11:35 +0300

    add the ability to create pastes for unauthorized users

--- voidpaste/models.py
+++ voidpaste/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Paste(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(
