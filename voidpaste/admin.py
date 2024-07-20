0d0da9e2d2f20b3b3b766c653a5aa58584a0e4d5
jpdreamthug <lentorilp@gmail.com>
2024-07-20 13:52:33 +0300

    register all models into admin panel and remove Group

--- voidpaste/admin.py
+++ voidpaste/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from voidpaste.models import Category, Paste, Comment, User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ...
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Paste)
+class PasteAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "category", "created_at", "delete_at", "link")
+    date_hierarchy = "created_at"
+    list_display_links = ("title", )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.unregister(Group)
