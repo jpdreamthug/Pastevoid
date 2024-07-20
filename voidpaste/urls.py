da42ca37f21eb051ee9d1f582058b5947563b48c
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-20 17:32:15 +0300

    add path to custom login page and to delete

--- voidpaste/urls.py
+++ voidpaste/urls.py
@@ -1,7 +1,13 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, re_path
 
-from voidpaste.views import PasteCreateView, PasteDetailView, ProfileListView, PasteUpdateView
+from voidpaste.views import (
+    PasteCreateView,
+    PasteDetailView,
+    ProfileListView,
+    PasteUpdateView,
+    PasteDeleteView, CustomLoginView,
+)
 
 urlpatterns = [
     path("", PasteCreateView.as_view(), name="index"),
@@ -10,14 +16,19 @@ urlpatterns = [
         PasteDetailView.as_view(),
         name="paste-detail"
     ),
-
     re_path(
         r"^(?P<link>[a-zA-Z0-9]{8})/edit/$",
         PasteUpdateView.as_view(),
         name="paste-edit"
     ),
+    re_path(
+        r"^(?P<link>[a-zA-Z0-9]{8})/delete/$",
+        PasteDeleteView.as_view(),
+        name="paste-delete"
+    ),
     path('profile/', ProfileListView.as_view(), name='profile'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login')
 ]
 
 app_name = "voidpaste"
