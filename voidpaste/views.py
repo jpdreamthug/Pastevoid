5c7c29ad90e0a57bfd395e010b3948f672d252dd
jpdreamthug <lentorilp@gmail.com>
2024-07-20 14:02:08 +0300

    add functionality of save form by authorized user and add profile view, update form view

--- voidpaste/views.py
+++ voidpaste/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
 
 from voidpaste.forms import PasteForm
 from voidpaste.models import Paste
@@ -10,6 +17,11 @@ class PasteCreateView(CreateView):
     form_class = PasteForm
     template_name = "voidpaste/index.html"
 
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.user = self.request.user
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse(
             "voidpaste:paste-detail",
@@ -19,7 +31,23 @@ class PasteCreateView(CreateView):
 
 class PasteDetailView(DetailView):
     model = Paste
-    template_name = "voidpaste/paste_detail.html"
-    context_object_name = "paste"
     slug_field = "link"
     slug_url_kwarg = "link"
+
+
+class ProfileListView(LoginRequiredMixin, ListView):
+    model = Paste
+    template_name = "voidpaste/profile.html"
+    context_object_name = "pastes"
+
+    def get_queryset(self):
+        return Paste.objects.filter(user=self.request.user)
+
+
+class PasteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Paste
+    fields = ("content",)
+
+
+class PasteDeleteView(LoginRequiredMixin, DeleteView):
+    ...
