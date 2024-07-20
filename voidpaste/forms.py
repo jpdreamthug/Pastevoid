8f4f39bc9789feca4ed562777274cad32e8c52fb
jpdreamthug <lentorilp@gmail.com>
2024-07-20 13:59:19 +0300

    refactor main form labels, placeholders and size of form

--- voidpaste/forms.py
+++ voidpaste/forms.py
@@ -1,9 +1,22 @@
 from django import forms
-
 from voidpaste.models import Paste
 
 
 class PasteForm(forms.ModelForm):
     class Meta:
         model = Paste
-        fields = ["title", "content", "category", "delete_choice"]
+        fields = ["content", "title", "category", "delete_choice"]
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'placeholder': 'Enter your Paste, probably Void',
+                'rows': 20,
+                'cols': 50
+            }),
+            'title': forms.TextInput(attrs={'placeholder': 'Enter title here'}),
+            'category': forms.Select(attrs={'placeholder': 'Select a category'}),
+        }
+        labels = {
+            'content': '',
+            'title': "Paste name/Title",
+            "delete_choice": "Expires at"
+        }
