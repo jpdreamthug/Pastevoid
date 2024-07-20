9fdfe96b069c6cb71cf0088076d322c0056883aa
jpdreamthug <lentorilp@gmail.com>
2024-07-20 13:56:10 +0300

    add paths to update paste, profile and custom logout

--- voidpaste/urls.py
+++ voidpaste/urls.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, re_path
 
-from voidpaste.views import PasteCreateView, PasteDetailView
+from voidpaste.views import PasteCreateView, PasteDetailView, ProfileListView, PasteUpdateView
 
 urlpatterns = [
     path("", PasteCreateView.as_view(), name="index"),
@@ -9,6 +10,14 @@ urlpatterns = [
         PasteDetailView.as_view(),
         name="paste-detail"
     ),
+
+    re_path(
+        r"^(?P<link>[a-zA-Z0-9]{8})/edit/$",
+        PasteUpdateView.as_view(),
+        name="paste-edit"
+    ),
+    path('profile/', ProfileListView.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 app_name = "voidpaste"
