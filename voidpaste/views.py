70a6b5e959e512ba8dcd8b0b6643d215e782ee33
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-21 16:00:53 +0300

    add pagination, register view and fix minor issue

--- voidpaste/views.py
+++ voidpaste/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -7,10 +9,14 @@ from django.views.generic import (
     DetailView,
     ListView,
     UpdateView,
-    DeleteView
+    DeleteView,
 )
 
-from voidpaste.forms import PasteForm
+from voidpaste.forms import (
+    PasteForm,
+    CustomUserCreationForm,
+    CommentForm
+)
 from voidpaste.models import Paste, Comment
 
 
@@ -37,8 +43,14 @@ class PasteDetailView(DetailView):
     slug_url_kwarg = "link"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        context["comments"] = Comment.objects.select_related("user").select_related("paste")
+        context = super().get_context_data(**kwargs)
+        comments = self.object.comments.all().order_by("-created_at")
+        paginator = Paginator(comments, 5)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context["comments"] = page_obj
+        context["comment_form"] = CommentForm()
+        context["page_obj"] = page_obj
         return context
 
 
@@ -46,9 +58,12 @@ class ProfileListView(LoginRequiredMixin, ListView):
     model = Paste
     template_name = "voidpaste/profile.html"
     context_object_name = "pastes"
+    paginate_by = 5
 
     def get_queryset(self):
-        return Paste.objects.filter(user=self.request.user)
+        return Paste.objects.filter(
+            user=self.request.user
+        ).order_by("-created_at")
 
 
 class PasteUpdateView(LoginRequiredMixin, UpdateView):
@@ -66,8 +81,8 @@ class PasteUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields.pop('category', None)
-        form.fields.pop('delete_choice', None)
+        form.fields.pop("category", None)
+        form.fields.pop("delete_choice", None)
         return form
 
 
@@ -75,7 +90,7 @@ class PasteDeleteView(LoginRequiredMixin, DeleteView):
     model = Paste
 
     def get_success_url(self):
-        return reverse_lazy('voidpaste:profile')
+        return reverse_lazy("voidpaste:profile")
 
     def get_object(self, queryset=None):
         queryset = Paste.objects.all()
@@ -85,14 +100,50 @@ class PasteDeleteView(LoginRequiredMixin, DeleteView):
         return get_object_or_404(queryset, link=link)
 
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "voidpaste/paste_detail.html"
+
+    def form_valid(self, form):
+        paste = get_object_or_404(Paste, link=self.kwargs["link"])
+        form.instance.paste = paste
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse(
+            "voidpaste:paste-detail",
+            kwargs={"link": self.kwargs["link"]}
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paste = get_object_or_404(Paste, link=self.kwargs["link"])
+        context["paste"] = paste
+        context["comments"] = paste.comments.all().order_by("-created_at")
+        return context
+
+
 class CustomLoginView(LoginView):
     template_name = "registration/login.html"
     success_url = reverse_lazy("voidpaste:index")
 
     def form_valid(self, form):
-        remember_me = self.request.POST.get('remember_me')
+        remember_me = self.request.POST.get("remember_me")
         if not remember_me:
             self.request.session.set_expiry(0)
         else:
             self.request.session.set_expiry(1209600)
         return super().form_valid(form)
+
+
+class RegisterView(CreateView):
+    template_name = "registration/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("voidpaste:index")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
