183f2ce21b7248fc566122614c91186095522791
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-21 15:59:36 +0300

    add registration path, form and template

--- voidpaste/forms.py
+++ voidpaste/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from voidpaste.models import Paste
+from django.contrib.auth.forms import UserCreationForm
+
+from voidpaste.models import Paste, User, Comment
 
 
 class PasteForm(forms.ModelForm):
@@ -7,16 +9,59 @@ class PasteForm(forms.ModelForm):
         model = Paste
         fields = ["content", "title", "category", "delete_choice"]
         widgets = {
-            'content': forms.Textarea(attrs={
-                'placeholder': 'Enter your Paste, probably Void',
-                'rows': 20,
-                'cols': 50
-            }),
-            'title': forms.TextInput(attrs={'placeholder': 'Enter title here'}),
-            'category': forms.Select(attrs={'placeholder': 'Select a category'}),
+            "content": forms.Textarea(
+                attrs={
+                    "placeholder": "Enter your Paste, probably Void",
+                    "rows": 20,
+                    "cols": 50,
+                }
+            ),
+            "title": forms.TextInput(
+                attrs={"placeholder": "Enter title here"}
+            ),
+            "category": forms.Select(
+                attrs={"placeholder": "Select a category"}
+            ),
         }
         labels = {
-            'content': '',
-            'title': "Paste name/Title",
-            "delete_choice": "Expires at"
+            "content": "",
+            "title": "Paste name/Title",
+            "delete_choice": "Expires at",
         }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+        widgets = {
+            "content": forms.Textarea(
+                attrs={
+                    "rows": 3,
+                    "placeholder": "Type your comment here...",
+                    "class": "form-control",
+                }
+            ),
+        }
+        labels = {
+            "content": "",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["content"].widget.attrs.update({"autofocus": "autofocus"})
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- voidpaste/urls.py
+++ voidpaste/urls.py
@@ -8,28 +8,34 @@ from voidpaste.views import (
     PasteUpdateView,
     PasteDeleteView,
     CustomLoginView,
+    RegisterView,
+    CommentCreateView,
 )
 
 urlpatterns = [
     path("", PasteCreateView.as_view(), name="index"),
+    path("profile/", ProfileListView.as_view(), name="profile"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("register/", RegisterView.as_view(), name="register"),
     re_path(
-        r"^(?P<link>[a-zA-Z0-9]{8})/$",
-        PasteDetailView.as_view(),
-        name="paste-detail"
+        r"^(?P<link>[a-zA-Z0-9]{8})/$", PasteDetailView.as_view(), name="paste-detail"
     ),
     re_path(
         r"^(?P<link>[a-zA-Z0-9]{8})/edit/$",
         PasteUpdateView.as_view(),
-        name="paste-edit"
+        name="paste-edit",
     ),
     re_path(
         r"^(?P<link>[a-zA-Z0-9]{8})/delete/$",
         PasteDeleteView.as_view(),
-        name="paste-delete"
+        name="paste-delete",
     ),
-    path('profile/', ProfileListView.as_view(), name='profile'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('login/', CustomLoginView.as_view(), name='login')
+    re_path(
+        r"^(?P<link>[a-zA-Z0-9]{8})/comment/$",
+        CommentCreateView.as_view(),
+        name="add-comment",
+    ),
 ]
 
 app_name = "voidpaste"
